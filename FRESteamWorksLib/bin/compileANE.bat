9d08e0a4e2063b2636ffaff3e5e7e76790135823
Ventero <ventero@ventero.de>
2012-10-27 22:19:31 +0200

    Rebuild the SWC when building the ANE

--- FRESteamWorksLib/bin/compileANE.bat
+++ FRESteamWorksLib/bin/compileANE.bat
@@ -1,6 +1,8 @@
 SET AIR_SDK=%USERPROFILE%\Documents\AdobeAIRSDK
+set FLEX_SDK=%ProgramFiles(x86)%\FlashDevelop\Tools\flexsdk
 set UNZIP_DIR=%ProgramFiles(x86)%\GnuWin32\bin
 
+"%FLEX_SDK%\bin\compc" +configname=air -source-path ..\src -optimize -include-classes com.amanitadesign.steam.FRESteamWorks com.amanitadesign.steam.SteamConstants -swf-version=11 -output FRESteamWorksLib.swc
 "%UNZIP_DIR%\unzip" -o FRESteamWorksLib.swc
 call "%AIR_SDK%\bin\adt" -package -target ane FRESteamWorks.ane descriptor.xml -swc FRESteamWorksLib.swc -platform Windows-x86 library.swf FRESteamWorks.dll -platform MacOS-x86 library.swf FRESteamWorks.framework
 del library.swf
--- FRESteamWorksLib/bin/compileANE.sh
+++ FRESteamWorksLib/bin/compileANE.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 AIR_SDK=/Developer/SDKs/AdobeAIRSDK
+FLEX_SDK=/Developer/SDKs/FlexSDK
 
 rm -R ./FRESteamWorks.framework
 cp -R ../../FRESteamWorks/build/Release/ ./
@@ -9,6 +10,7 @@ install_name_tool -change \
     "@loader_path/libsteam_api.dylib" "@rpath/../Resources/libsteam_api.dylib" \
     ./FRESteamWorks.framework/FRESteamWorks
 
+"$FLEX_SDK/bin/compc" +configname=air -source-path ..\src -optimize -include-classes com.amanitadesign.steam.FRESteamWorks com.amanitadesign.steam.SteamConstants -swf-version=11 -output FRESteamWorksLib.swc
 unzip -o FRESteamWorksLib.swc
 "$AIR_SDK"/bin/adt -package -target ane FRESteamWorks.ane descriptor.xml -swc FRESteamWorksLib.swc -platform Windows-x86 library.swf FRESteamWorks.dll -platform MacOS-x86 library.swf FRESteamWorks.framework
 rm library.swf
