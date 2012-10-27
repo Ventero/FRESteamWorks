0e20ff832a5e9443588cd3dbde3d74acb896107e
Ventero <ventero@ventero.de>
2012-10-27 21:48:58 +0200

    Change a few more paths

--- FRESteamWorksTest/bin-debug/packageWin.bat
+++ FRESteamWorksTest/bin-debug/packageWin.bat
@@ -1,4 +1,4 @@
-SET AIR_SDK=C:\AirSDK\air_3.2
+SET AIR_SDK=%USERPROFILE%\Documents\AdobeAIRSDK
 SET ANE_PATH=..\..\FRESteamWorksLib\bin
 
 call "%AIR_SDK%\bin\adt.bat" -package -XnoAneValidate -tsa none -storetype pkcs12 -keystore CertificateTest.p12 -storepass test -target bundle FRESteamWorksTest  FRESteamWorksTest-app.xml FRESteamWorksTest.swf steam_api.dll libsteam_api.dylib steam_appid.txt -extdir "%ANE_PATH%"
