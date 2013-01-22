aaa767949fa32176d05f26ee2d5801a440498cff
Ventero <ventero@ventero.de>
2013-01-22 23:12:38 +0100

    Use external Steam API library file

--- FRESteamWorksTest/bin-debug/packageMac.sh
+++ FRESteamWorksTest/bin-debug/packageMac.sh
@@ -2,12 +2,17 @@
 set -x
 
 AIR_SDK=../../../AdobeAIRSDK
+STEAM_SDK=../../../Steam/sdk
 ANE_PATH=../../FRESteamWorksLib/bin
+
+cp "$STEAM_SDK/redistributable_bin/steam_api.dll" .
+cp "$STEAM_SDK/redistributable_bin/osx32/libsteam_api.dylib" .
 
 "$AIR_SDK"/bin/adt -package -XnoAneValidate -tsa none -storetype pkcs12 \
                    -keystore CertificateTest.p12 -storepass test \
                    -target bundle FRESteamWorksTest FRESteamWorksTest-app.xml \
-                    FRESteamWorksTest.swf steam_api.dll libsteam_api.dylib \
-                    steam_appid.txt -extdir "$ANE_PATH"
+                   FRESteamWorksTest.swf \
+                   steam_api.dll libsteam_api.dylib steam_appid.txt \
+                   -extdir "$ANE_PATH"
 
-
+rm -f steam_api.dll libsteam_api.dylib
--- FRESteamWorksTest/bin-debug/packageWin.bat
+++ FRESteamWorksTest/bin-debug/packageWin.bat
@@ -1,6 +1,15 @@
 SET AIR_SDK=%USERPROFILE%\Documents\AdobeAIRSDK
+set STEAM_SDK=%USERPROFILE%\Documents\Steam\sdk
 SET ANE_PATH=..\..\FRESteamWorksLib\bin
 
-call "%AIR_SDK%\bin\adt.bat" -package -XnoAneValidate -tsa none -storetype pkcs12 -keystore CertificateTest.p12 -storepass test -target bundle FRESteamWorksTest  FRESteamWorksTest-app.xml FRESteamWorksTest.swf steam_api.dll libsteam_api.dylib steam_appid.txt -extdir "%ANE_PATH%"
+copy "%STEAM_SDK%\redistributable_bin\steam_api.dll" .
+copy "%STEAM_SDK%\redistributable_bin\osx32\libsteam_api.dylib" .
 
-call "FRESteamWorksTest\FRESteamWorksTest.exe"
+call "%AIR_SDK%\bin\adt.bat" -package -XnoAneValidate -tsa none ^
+                             -storetype pkcs12 -keystore CertificateTest.p12 ^
+                             -storepass test -target bundle FRESteamWorksTest ^
+                             FRESteamWorksTest-app.xml FRESteamWorksTest.swf ^
+                             steam_api.dll libsteam_api.dylib steam_appid.txt^
+                             -extdir "%ANE_PATH%"
+
+del steam_api.dll libsteam_api.dylib
