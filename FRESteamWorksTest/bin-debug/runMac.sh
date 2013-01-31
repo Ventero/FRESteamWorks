a489d77f0678f0d79628e09da6ad500efdd0ede8
Ventero <ventero@ventero.de>
2013-02-01 00:26:18 +0100

    Use DYLD_FALLBACK_LIBRARY_PATH for Mac run script.
    This prevents the runtime linker from picking up the libraries in the
    SDK directory instead of the actual client directory.

--- FRESteamWorksTest/bin-debug/runMac.sh
+++ FRESteamWorksTest/bin-debug/runMac.sh
@@ -1,5 +1,5 @@
 AIR_SDK=../../../AdobeAIRSDK
 STEAM_SDK=../../../Steam/sdk
 
-export DYLD_LIBRARY_PATH="$STEAM_SDK/redistributable_bin/osx32"
+export DYLD_FALLBACK_LIBRARY_PATH="$STEAM_SDK/redistributable_bin/osx32"
 "$AIR_SDK/bin/adl" -extdir ../../FRESteamWorksLib/bin/ FRESteamWorksTest-app.xml .
