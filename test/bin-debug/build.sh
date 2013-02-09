19ad57c74d53a31339bc8c706f83e7e74b0ba88d
Ventero <ventero@ventero.de>
2013-02-09 23:26:00 +0100

    Build test app as SWF version 11

--- test/bin-debug/build.sh
+++ test/bin-debug/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 FLEXSDK="../../../FlexSDK"
-"$FLEXSDK/bin/amxmlc" -compiler.source-path=../../lib/src -output FRESteamWorksTest.swf ../src/FRESteamWorksTest.as
+"$FLEXSDK/bin/amxmlc" -swf-version=11 -compiler.source-path=../../lib/src -output FRESteamWorksTest.swf ../src/FRESteamWorksTest.as
