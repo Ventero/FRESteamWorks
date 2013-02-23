d7599033596737c25d80d1c2281f3884b1e56829
Ventero <ventero@ventero.de>
2013-02-23 14:01:36 +0100

    Whitespace

--- test/bin-debug/packageWin.bat
+++ test/bin-debug/packageWin.bat
@@ -9,7 +9,7 @@ call "%AIR_SDK%\bin\adt.bat" -package -XnoAneValidate -tsa none ^
                              -storetype pkcs12 -keystore CertificateTest.p12 ^
                              -storepass test -target bundle FRESteamWorksTest ^
                              FRESteamWorksTest-app.xml FRESteamWorksTest.swf ^
-                             steam_api.dll libsteam_api.dylib steam_appid.txt^
+                             steam_api.dll libsteam_api.dylib steam_appid.txt ^
                              -extdir "%ANE_PATH%"
 
 del steam_api.dll libsteam_api.dylib
