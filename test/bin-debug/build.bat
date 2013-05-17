ba85f5248861ea15146a10503363d90b6d19e6b3
Ventero <ventero@ventero.de>
2013-05-17 15:41:32 +0200

    Add Windows build script for test binary

---
+++ test/bin-debug/build.bat
@@ -0,0 +1,9 @@
+
+if not exist ..\..\config.bat (
+    echo "FRESteamWorks/config.bat is not set up!"
+    goto :eof
+)
+call ..\..\config.bat
+
+"%FLEX_SDK%/bin/amxmlc" -swf-version=11 -compiler.source-path=../../lib/src ^
+                        -output FRESteamWorksTest.swf ../src/FRESteamWorksTest.as
