17b8d5314e2264cc3953a329a8ac371bbe09d58c
Ventero <ventero@ventero.de>
2013-05-23 01:24:45 +0200

    Port API script to Ruby 1.8

--- lib/API_to_lib.rb
+++ lib/API_to_lib.rb
@@ -1,19 +1,21 @@
-#!/usr/bin/ruby1.9.1
+#!/usr/bin/ruby
 
 contents = open("API.txt", "r").read.split("\n")
 
 contents.each do |line|
-	if line.empty? or line[0] == "/"
+	if line.empty? or line[0].chr == "/"
 		puts "\t\t#{line}".rstrip
 		next
 	end
 
 	func = line.split("(")[0].split[-1]
+
 	ret = line.split(":")[-1]
 	args = line[/\(([^)]*)\)/, 1]
 	arg_names = args.split(/,\s*/).map do |arg|
 		arg.split(":")[0]
-	end.unshift("\"AIRSteam_#{func[0].upcase + func[1,func.size]}\"")
+	end.unshift("\"AIRSteam_#{func[0].chr.upcase + func[1,func.size]}\"")
+
 	puts <<EOD
 		#{line}
 		{
