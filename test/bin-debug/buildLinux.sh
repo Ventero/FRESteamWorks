db07ab85f609084ebdd57ddde3cf19689da0de4c
Ventero <ventero@ventero.de>
2013-08-28 20:08:35 +0200

    Compile APIWrapper when building Linux test tool.

--- test/bin-debug/buildLinux.sh
+++ test/bin-debug/buildLinux.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -e
+
 [ ! -e ../../config.sh ] && die "FRESteamWorks/config.sh is not set up!"
 . ../../config.sh
+
+make -C ../../src wrapper
+cp ../../src/APIWrapper/APIWrapper NativeApps/Linux
 
 "$FLEX_SDK/bin/amxmlc" -library-path+=../../lib/bin/FRESteamWorksLibLinux.swc \
                        -swf-version=11 -output FRESteamWorksTest_linux.swf \
