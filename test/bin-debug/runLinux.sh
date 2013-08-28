a6c0897abb7f56a81507d8ccedfc0b981948b0fa
Ventero <ventero@ventero.de>
2013-08-28 20:43:32 +0200

    Setting LD_PRELOAD isn't required anymore.

--- test/bin-debug/runLinux.sh
+++ test/bin-debug/runLinux.sh
@@ -4,7 +4,6 @@
 . ../../config.sh
 
 export LD_PRELOAD="$STEAM_SDK/redistributable_bin/linux32/libsteam_api.so"
-export LD_LIBRARY_PATH="$HOME/.steam/bin"
 
 if [ -z "$SteamAppId" ] && [ ! -e "steam_appid.txt" ]; then
 	echo "\$SteamAppId not set and steam_appid.txt not found"
@@ -14,11 +13,6 @@ fi
 if [ ! -e "$LD_PRELOAD" ]; then
 	echo "libsteam_api.so not found"
 	exit 2
-fi
-
-if [ ! -e "$LD_LIBRARY_PATH/libsteam.so" ]; then
-	echo "libsteam.so not found"
-	exit 3
 fi
 
 if [ ! -x "NativeApps/Linux/APIWrapper" ]; then
