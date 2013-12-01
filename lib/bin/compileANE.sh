9275f4c1f54bac0af69ede8acc3589ec1b266933
Ventero <ventero@ventero.de>
2013-12-01 18:58:00 +0100

    Work around broken symlink on OS X.

--- lib/bin/compileANE.sh
+++ lib/bin/compileANE.sh
@@ -7,6 +7,9 @@ set -xe
 install_name_tool -change \
     "@loader_path/libsteam_api.dylib" "@rpath/../Resources/libsteam_api.dylib" \
     ./FRESteamWorks.framework/FRESteamWorks
+# XCode on OS X Mavericks creates frameworks with a broken symlink
+# Create that link's target so adt doesn't complain later on
+mkdir -p FRESteamWorks.framework/Versions/Current/Resources
 
 "$FLEX_SDK/bin/compc" +configname=air -source-path ../src -optimize \
                       -include-sources ../src/ \
@@ -14,6 +17,11 @@ install_name_tool -change \
 
 unzip -o FRESteamWorksLib.swc
 trap "rm -f library.swf catalog.xml" EXIT
+
+if [ ! -f FRESteamWorks.dll ]; then
+	echo "Creating dummy FRESteamWorks.dll"
+	touch FRESteamWorks.dll
+fi
 
 "$AIR_SDK"/bin/adt -package -target ane FRESteamWorks.ane descriptor.xml \
                    -swc FRESteamWorksLib.swc \
