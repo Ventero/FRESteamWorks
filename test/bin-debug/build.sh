a332cae9a1c96cf87ee5d82081607209874d56a5
Ventero <ventero@ventero.de>
2013-12-28 19:34:21 +0100

    Fix SWF version typo.

--- test/bin-debug/build.sh
+++ test/bin-debug/build.sh
@@ -2,4 +2,4 @@
 [ ! -e ../../config.sh ] && die "FRESteamWorks/config.sh is not set up!"
 . ../../config.sh
 
-"$FLEX_SDK/bin/amxmlc" -swf-version=21 -external-library-path+=../../lib/bin/FRESteamWorks.ane -output FRESteamWorksTest.swf ../src/FRESteamWorksTest.as
+"$FLEX_SDK/bin/amxmlc" -swf-version=11 -external-library-path+=../../lib/bin/FRESteamWorks.ane -output FRESteamWorksTest.swf ../src/FRESteamWorksTest.as
