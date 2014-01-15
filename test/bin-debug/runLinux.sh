d3c6cd3b44dd64365fdca27de831fa3fb81d109f
Ventero <ventero@ventero.de>
2014-01-15 21:49:09 +0100

    Support additional LD_PRELOADs in Linux run script.

--- test/bin-debug/runLinux.sh
+++ test/bin-debug/runLinux.sh
@@ -3,14 +3,14 @@
 [ ! -e ../../config.sh ] && die "FRESteamWorks/config.sh is not set up!"
 . ../../config.sh
 
-export LD_PRELOAD="$STEAM_SDK/redistributable_bin/linux32/libsteam_api.so"
+STEAM_LIB="$STEAM_SDK/redistributable_bin/linux32/libsteam_api.so"
 
 if [ -z "$SteamAppId" ] && [ ! -e "steam_appid.txt" ]; then
 	echo "\$SteamAppId not set and steam_appid.txt not found"
 	exit 1
 fi
 
-if [ ! -e "$LD_PRELOAD" ]; then
+if [ ! -e "$STEAM_LIB" ]; then
 	echo "libsteam_api.so not found"
 	exit 2
 fi
@@ -21,4 +21,5 @@ if [ ! -x "NativeApps/Linux/APIWrapper" ]; then
 	exit 4
 fi
 
+export LD_PRELOAD="$STEAM_LIB:${LD_PRELOAD+:$LD_PRELOAD}"
 "$AIR_SDK/bin/adl" FRESteamWorksTest-linux.xml .
