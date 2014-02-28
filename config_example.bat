7ffd424fff737c8bcbeeeb44c49d18435ec41395
Ventero <ventero@ventero.de>
2014-02-28 03:52:36 +0100

    More explanation in example configs.

--- config_example.bat
+++ config_example.bat
@@ -1,11 +1,12 @@
-REM absolute paths to all the relevant directories and binaries
+REM To set up your config on Windows, copy this file to config.bat and change
+REM the values below accordingly. All paths must be absolute paths!
 
-REM path to the main AIR SDK directory, so that %AIR_SDK%\bin\adl exists
+REM Path to the main AIR SDK directory, so that %AIR_SDK%\bin\adl exists.
 set "AIR_SDK=%USERPROFILE%\Documents\AdobeAIRSDK"
-REM path to the FLEX SDK directory, so that %FLEX_SDK%\bin\compc exists
-REM might be identical to AIR_SDK
+REM Path to the FLEX SDK directory, so that %FLEX_SDK%\bin\compc exists.
+REM Might be identical to AIR_SDK if you use an overlayed SDK.
 set "FLEX_SDK=%ProgramFiles(x86)%\FlashDevelop\Tools\flexsdk"
-REM path to the Steam SDK directory, so that %STEAM_SDK%\redistributable_bin exist
+REM Path to the Steam SDK, so that %STEAM_SDK%\redistributable_bin exists.
 set "STEAM_SDK=%USERPROFILE%\Documents\Steam\sdk"
-REM path to the unzip binary
+REM Path to the unzip binary. Only needed when building the ANE.
 set "UNZIP_BIN=%ProgramFiles(x86)%\GnuWin32\bin\unzip"
--- config_example.sh
+++ config_example.sh
@@ -1,14 +1,16 @@
-# all paths must be absolute paths
-# on Linux, remember to wrap all variables in ${}, so that they can
-# be used in the Makefile
-# path to the main AIR SDK directory, so that $AIR_SDK/bin/adt exists
+# To set up your config on OS X or Linux, copy this file to config.sh and
+# change the values below accordingly. All paths must be absolute paths!
+# On Linux, remember to wrap all variables in ${}, so that they can
+# be used in the Makefile.
+
+# Path to the main AIR SDK directory, so that $AIR_SDK/bin/adt exists.
 AIR_SDK="${HOME}/Coding/AdobeAIRSDK"
-# path to the Flex SDK, so that $FLEX_SDK/bin/compc exists
-# might be identical to AIR_SDK
+# Path to the Flex SDK, so that $FLEX_SDK/bin/compc exists.
+# Might be identical to AIR_SDK if you use an overlayed SDK.
 FLEX_SDK="${HOME}/Coding/FlexSDK"
-# path to the Steam SDK, so that $STEAM_SDK/redistributable_bin exists
+# Path to the Steam SDK, so that $STEAM_SDK/redistributable_bin exists.
 STEAM_SDK="${HOME}/Coding/SteamSDK"
-# ftp path to upload the built binaries to when running builds/build.sh
-# version number gets appended to the path. set it to an empty value to not
-# upload the binaries.
-UPLOAD_URL="ftp://example.org/FRESteamWorks"
+# FTP path to upload the built binaries to when running builds/build.sh.
+# Version number gets appended to the path. Binaries will not be uploaded
+# if left commented or set to an empty value.
+#UPLOAD_URL="ftp://example.org/FRESteamWorks"
