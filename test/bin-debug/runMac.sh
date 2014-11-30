ad1329439d5b66bea4e475d834cfce9b576cd1ef
Ventero <ventero@ventero.de>
2014-11-30 17:11:19 +0100

    Fix path to gameoverlayrenderer.dylib in OS X run script.

--- test/bin-debug/runMac.sh
+++ test/bin-debug/runMac.sh
@@ -4,5 +4,5 @@
 . ../../config.sh
 
 export DYLD_FALLBACK_LIBRARY_PATH="$STEAM_SDK/redistributable_bin/osx32"
-export DYLD_INSERT_LIBRARIES=/Applications/Steam.app/Contents/MacOS/osx32/gameoverlayrenderer.dylib
+export DYLD_INSERT_LIBRARIES=/Applications/Steam.app/Contents/MacOS/gameoverlayrenderer.dylib
 "$AIR_SDK/bin/adl" -extdir ../../lib/bin/ FRESteamWorksTest-app.xml . -- "$@"
