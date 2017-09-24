bc558a8f54c908dba3c615f8785d53d2937b0543
Ventero <ventero@ventero.de>
2017-09-24 23:30:02 +0100

    Check another location for overlay library.

--- test/bin-debug/runMac.sh
+++ test/bin-debug/runMac.sh
@@ -4,5 +4,17 @@
 . ../../config.sh
 
 export DYLD_FALLBACK_LIBRARY_PATH="$STEAM_SDK/redistributable_bin/osx32"
-export DYLD_INSERT_LIBRARIES=/Applications/Steam.app/Contents/MacOS/gameoverlayrenderer.dylib
+
+# Ensure the overlay is added when running outside Steam
+overlay_libs=(
+    "/Library/Application Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/gameoverlayrenderer.dylib"
+    "/Applications/Steam.app/Contents/MacOS/gameoverlayrenderer.dylib"
+)
+for file in "${overlay_libs[@]}"; do
+    if [ -f "$file" ]; then
+        DYLD_INSERT_LIBRARIES="$file${DYLD_INSERT_LIBRARIES+:$DYLD_INSERT_LIBRARIES}"
+    fi
+done
+export DYLD_INSERT_LIBRARIES
+
 "$AIR_SDK/bin/adl" -extdir ../../lib/bin/ FRESteamWorksTest-app.xml . -- "$@"
